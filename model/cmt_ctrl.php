79b364a69bce385795e9bc1a186fd7c822a3d694
ggugguiscute <kimgukwoo@gmail.com>
2023-01-03 12:23:37 +0900

    0103 comment modify

--- model/cmt_ctrl.php
+++ model/cmt_ctrl.php
@@ -2,8 +2,45 @@
 
 // select spl_cmt.* from spl_cmt join spl_user on spl_cmt.cmt_u_idx = spl_user.user_idx where cmt_pro_idx = 16
 
-// include $_SERVER['DOCUMENT_ROOT'].'/main_backend/etc/error.php';
+include $_SERVER['DOCUMENT_ROOT'].'/main_backend/etc/error.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/main_backend/connect/dbconn.php';
+
+function parse_raw_http_request(array &$a_data)
+{
+  // read incoming data
+  $input = file_get_contents('php://input');
+  
+  // grab multipart boundary from content type header
+  preg_match('/boundary=(.*)$/', $_SERVER['CONTENT_TYPE'], $matches);
+  $boundary = $matches[1];
+  
+  // split content by boundary and get rid of last -- element
+  $a_blocks = preg_split("/-+$boundary/", $input);
+  array_pop($a_blocks);
+      
+  // loop data blocks
+  foreach ($a_blocks as $id => $block)
+  {
+    if (empty($block))
+      continue;
+    
+    // you'll have to var_dump $block to understand this and maybe replace \n or \r with a visibile char
+    
+    // parse uploaded files
+    if (strpos($block, 'application/octet-stream') !== FALSE)
+    {
+      // match "name", then everything after "stream" (optional) except for prepending newlines 
+      preg_match('/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s', $block, $matches);
+    }
+    // parse all other fields
+    else
+    {
+      // match "name" and optional value in between newline sequences
+      preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
+    }
+    $a_data[$matches[1]] = $matches[2];
+  }        
+}
 
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['req_sign']) && $_GET['req_sign'] == 'post_cmt'){
     post_cmt($conn);
@@ -56,10 +93,16 @@ function post_cmt($conn){
     // echo json_encode(array("u_idx" => $u_idx, "pro_idx" => $pro_idx, "content" => $content, "cmt_reg" => $cmt_reg)); 
 }
 // 상품조회
-function get_cmt($conn){
+    function get_cmt($conn){
 
     $p_idx = $_GET['p_idx'];
-    $userid = $_SESSION['userid'];
+
+    if(isset($_SESSION['userid'])){
+        $userid = $_SESSION['userid'];
+    }else{
+        $userid = "guest";
+    }
+
 
     //spl_cmt 테이블 전체 테이터와 spl_user의 아이디를 조회한다.(두 개 테이블 데이터를 동시에 조회하기 위해서 테이블 간 join이 필요하다.)
     // join 참조 : https://pearlluck.tistory.com/46
@@ -74,12 +117,49 @@ function get_cmt($conn){
     }else{
         $json_result = array();//빈 배열 초기화
         while($row = mysqli_fetch_array($result)){
-        array_push($json_result, array("cmt_cont" => $row['cmt_cont'],"cmt_reg" => $row['cmt_reg'],"user_id" => $row['user_id'],"session_id" => $userid)); //첫번째 파라미터 : 대상배열, 두번째 파라미터 : 배열 입력값
+        array_push($json_result, array("cmt_idx" => $row['cmt_idx'],"cmt_cont" => $row['cmt_cont'],"cmt_reg" => $row['cmt_reg'],"user_id" => $row['user_id'],"session_id" => $userid)); //첫번째 파라미터 : 대상배열, 두번째 파라미터 : 배열 입력값
     }
     }
 
     echo json_encode($json_result);
 }
-function patch_cmt($conn){}
+    function patch_cmt($conn){
+    // update 구문 참조 = http://www.tcpschool.com/mysql/mysql_basic_update
+
+    $_PATCH = [];
+    parse_str(file_get_contents('php://input'), $_PATCH);
+    parse_raw_http_request($_PATCH);  //undefined function
+    //php patch 관련 참조 : https://stackoverflow.com/questions/20572639/get-patch-request-data-in-php
+
+    $cmt_idx = $_GET['cmt_idx'];
+    $cmt_cont = $_PATCH['update_cont'];
+    // php에서는 공식적으로 post와 get만 지원한다. 따라서 patch, delete, put등은 별도의 접근 처리를 해주어야 한다.
+
+    if(!isset($_SESSION['useridx'])){
+        echo json_encode(array("msg" => "작성한 본인이 아니면 수정할 수 없습니다."));
+        exit();
+    }
+
+    $sql = "UPDATE spl_cmt SET cmt_cont = ? WHERE cmt_idx = ?";
+
+    $stmt = $conn->stmt_init();
+
+    if(!$stmt->prepare($sql)){
+        http_response_code(400);
+        echo json_encode(array("msg" => "상품평 수정에 실패하였습니다."));
+    }
+
+    $stmt -> bind_param("ss", $cmt_cont, $cmt_idx); //sql 순서 같게
+    $stmt -> execute();
+
+    if($stmt->affected_rows > 0){
+        http_response_code(200);
+        echo json_encode(array("msg" => "상품평이 수정되었습니다.")); 
+    }else{
+        http_response_code(400);
+        echo json_encode(array("msg" => "상품평 수정에 실패하였습니다.")); 
+    }
+    // echo json_encode(array("cmt_idx" => $cmt_idx, "cmt_cont" => $cmt_cont));
+}
 
 ?>
